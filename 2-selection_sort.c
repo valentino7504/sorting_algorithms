c71fc72b0429c129b5211e2b630a6bc47953a196
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-22 20:00:14 +0100

    Solved selection sort task

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array using selection sort
+ * @array: the array
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t first_unsorted = 0, i, j, min_index;
+	int minimum = array[first_unsorted], temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = first_unsorted; j < size; j++)
+		{
+			if (array[j] < minimum)
+			{
+				minimum = array[j];
+				min_index = j;
+			}
+		}
+		if (min_index != first_unsorted)
+		{
+			temp = array[first_unsorted];
+			array[first_unsorted] = minimum;
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+		first_unsorted++;
+		minimum = array[first_unsorted];
+		min_index = first_unsorted;
+	}
+}
