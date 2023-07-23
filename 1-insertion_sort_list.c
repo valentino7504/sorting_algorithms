3ceb3c4413ff2c1d414f14c8c7041d7fa1c0c293
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-23 20:40:27 +0100

    First solution for insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a linked list using
+ * insertion sort
+ * @list: the list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *unsorted = (*list)->next;
+	listint_t *next_node, *current_node;
+
+	while (unsorted != NULL)
+	{
+		next_node = unsorted->next;
+		current_node = unsorted->prev;
+		while (current_node != NULL)
+		{
+			if (unsorted->n < current_node->n)
+			{
+				if (current_node == *list)
+					*list = unsorted;
+				current_node->next = unsorted->next;
+				unsorted->next = current_node;
+				if (current_node->next != NULL)
+					current_node->next->prev = current_node;
+				unsorted->prev = current_node->prev;
+				current_node->prev = unsorted;
+				if (unsorted->prev != NULL)
+					unsorted->prev->next = unsorted;
+				print_list(*list);
+			}
+			else
+				break;
+			current_node = unsorted->prev;
+		}
+		unsorted = next_node;
+	}
+}
